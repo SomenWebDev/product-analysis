6bea695146ef05cfcefde6c1bce9bfab584b77a4
Somen Singha <somensingha06@gmail.com>
2022-04-05 00:13:55 +0600

    added

--- book-shop/src/components/About/About.js
+++ book-shop/src/components/About/About.js
@@ -1,7 +1,20 @@
 import React from "react";
 
 const About = () => {
-  return <div></div>;
+  return (
+    <div>
+      <p>
+        Bookque is an online bookstore with a mission to financially support
+        local, independent bookstores. We believe that bookstores are essential
+        to a healthy culture. They’re where authors can connect with readers,
+        where we discover new writers, where children get hooked on the thrill
+        of reading that can last a lifetime. They’re also anchors for our
+        downtowns and communities. As more and more people buy their books
+        online, we wanted to create an easy, convenient way for you to get your
+        books and support bookstores at the same time.
+      </p>
+    </div>
+  );
 };
 
 export default About;
